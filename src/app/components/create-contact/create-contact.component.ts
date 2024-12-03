b34913e5fdf68884ab33398c6493e1f0c4c1fcfc
Pijus Hazra <pijuskantihazra032@gmail.com>
2024-12-03 11:29:00 +0000

    create-contact features

--- src/app/components/create-contact/create-contact.component.ts
+++ src/app/components/create-contact/create-contact.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-contact.component.html',
   styleUrl: './create-contact.component.css'
 })
 export class CreateContactComponent {
+  form: any;
+  mobileRegex: string = '[6789][0-9]{9}';
+  emailRegex: string = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$';
 
+  constructor(private router: Router) {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      photo: new FormControl('', [Validators.required]),
+      mobile: new FormControl('', [Validators.required, Validators.pattern(this.mobileRegex)]),
+      email: new FormControl('', [Validators.pattern(this.emailRegex)]),
+      company: new FormControl('')
+    })
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+  get photo() {
+    return this.form.get('photo');
+  }
+
+  get mobile() {
+    return this.form.get('mobile');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
 }
